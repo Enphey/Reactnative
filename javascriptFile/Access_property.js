e622e8fcd303bd9045a92f0c4342ffffc5ec653f
Enphey <be.nathiga_st@tni.ac.th>
2023-06-24 11:31:50 +0700

    Java

---
+++ javascriptFile/Access_property.js
@@ -0,0 +1,10 @@
+let user ={
+    id:1,
+    name : "Noey"
+};
+
+console.log("ID: ",user.id);
+console.log("Name: ",user.name);
+
+user.name ="Enphey";
+console.log("Name: ",user.name)
--- javascriptFile/Function.js
+++ javascriptFile/Function.js
@@ -6,6 +6,12 @@
     return sum/ args.length;
 } */
 
-
+const avg =(...args) => {
+    let sum = 0;
+    for (const item of args){
+         sum+=item;
+    }
+    return sum/ args.length;
+}
 
 console.log(avg(2,3,4));
